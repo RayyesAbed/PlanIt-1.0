154de4d7ce324d5e5df4ee447a398c4667d3593c
RayyesAbed <abedray1567@gmail.com>
2024-12-24 20:01:04 +0100

    Returned a welcoming div and nested the image specified for that

--- frontend/src/components/welcomeSection/Welcome.jsx
+++ frontend/src/components/welcomeSection/Welcome.jsx
@@ -1,5 +1,12 @@
 import styles from "./Welcome.module.css";
+import WelcomeSectionImg from "/WelcomeSectionImg.jpg";
 
-const Welcome = () => {};
+const Welcome = () => {
+  return (
+    <div id={styles.welcomeDiv}>
+      <img src={WelcomeSectionImg} alt="Welcome Section Image" />
+    </div>
+  );
+};
 
 export default Welcome;
