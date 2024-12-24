355f364e07c6156e486d3eed4ca8144133d8167b
RayyesAbed <abedray1567@gmail.com>
2024-12-24 19:14:18 +0100

    Added content to the IntroNavbar Component
    - Added PlanIt Logo
    - Added the main ul
    - Added another ul for register and login
    - And added css ids to nav, main ul, and the other ul

--- frontend/src/components/introNavbar/IntroNavbar.jsx
+++ frontend/src/components/introNavbar/IntroNavbar.jsx
@@ -1,5 +1,23 @@
 import styles from "./IntroNavbar.module.css";
+import PlanItLogo from "/PlanItLogo.webp";
 
-const IntroNavbar = () => {};
+const IntroNavbar = () => {
+  return (
+    <nav id={styles.navbar}>
+      <a>
+        <img src={PlanItLogo} alt="PlanIt Logo" />
+      </a>
+      <ul id={styles.normalUl}>
+        <li>FEATURES</li>
+        <li>ABOUT</li>
+        <li>DOWNLOAD</li>
+      </ul>
+      <ul id={styles.registerLoginUl}>
+        <li>LOGIN</li>
+        <li>REGISTER</li>
+      </ul>
+    </nav>
+  );
+};
 
 export default IntroNavbar;
