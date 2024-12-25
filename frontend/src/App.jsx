f9429635e6168cb06e5360e35f2cecb2fa07cc4b
RayyesAbed <abedray1567@gmail.com>
2024-12-25 16:03:47 +0100

    Used React Router Browser Router, Routes, and Route to begin with PlanIt website routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Introduction from "./pages/introduction/Introduction";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Introduction />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
