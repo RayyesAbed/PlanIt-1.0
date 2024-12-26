2fad7fd19335778bb1cd7fa264ab96ccbc99fde8
RayyesAbed <abedray1567@gmail.com>
2024-12-26 18:02:33 +0100

    Created the Download Component to be used in Introduction
    - Imported the Download CSS module file
    - Imported the App Store Preorder image to be used
    - created 2 sections; one for newsletter signing and other for App Store Preorder button

---
+++ frontend/src/components/download/Download.jsx
@@ -0,0 +1,37 @@
+import styles from "./Download.module.css";
+import AppStorePreorder from "/AppStorePreorder.svg";
+
+const Download = () => {
+  return (
+    <div id="download" className={styles.aboutDiv}>
+      <div className={styles.wrapper}>
+        <section>
+          <h1>Downloads</h1>
+          <p>
+            Now you can use our website and create a new account and start your
+            journey!
+          </p>
+          <p>
+            An iOS app is currently under development and once it’s available,
+            we will notify you. Please enter your email for our newsletter here
+            below:
+          </p>
+          <div className={styles.newsletterWrapper}>
+            <input
+              type="email"
+              className={styles.newsletterInput}
+              name="email"
+              placeholder="Enter your Email"
+            />
+            <button>Sign Up</button>
+          </div>
+        </section>
+        <section>
+          <img src={AppStorePreorder} alt="App Store Preorder Image" />
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Download;
