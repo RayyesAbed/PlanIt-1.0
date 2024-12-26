70b6f8f6788da85d8388cc251eaa2d269f7e5502
RayyesAbed <abedray1567@gmail.com>
2024-12-26 13:26:48 +0100

    Imported HashLink and NavHashLink and used them inside IntroNavbar and other changes
    - Changed the Navbar background color depending on scroll event
    - Changed the anchor for the logo to be HashLink
    - Changed the anchor for the list items to be NavHashLink

--- frontend/src/components/introNavbar/IntroNavbar.jsx
+++ frontend/src/components/introNavbar/IntroNavbar.jsx
@@ -1,16 +1,45 @@
+import { useEffect, useState } from "react";
 import styles from "./IntroNavbar.module.css";
 import PlanItLogo from "/PlanItLogo.webp";
+import { NavHashLink, HashLink } from "react-router-hash-link";
 
 const IntroNavbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScrollY() {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", handleScrollY);
+
+    return () => window.removeEventListener("scroll", handleScrollY);
+  });
+
   return (
-    <nav id={styles.navbar}>
-      <a>
+    <nav
+      id={styles.navbar}
+      style={scrolled ? { backgroundColor: "black" } : {}}
+    >
+      <HashLink to="#welcome">
         <img src={PlanItLogo} alt="PlanIt Logo" />
-      </a>
+      </HashLink>
       <ul id={styles.normalUl}>
-        <li>FEATURES</li>
-        <li>ABOUT</li>
-        <li>DOWNLOAD</li>
+        <li>
+          <NavHashLink className={styles.navLinkLi} to="#features">
+            FEATURES
+          </NavHashLink>
+        </li>
+        <li>
+          <NavHashLink className={styles.navLinkLi}>ABOUT</NavHashLink>
+        </li>
+        <li>
+          <NavHashLink className={styles.navLinkLi}>DOWNLOAD</NavHashLink>
+        </li>
       </ul>
       <ul id={styles.registerLoginUl}>
         <li>LOGIN</li>
