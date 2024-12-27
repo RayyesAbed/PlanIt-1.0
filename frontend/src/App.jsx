136c4487b9da261d2bfd0c23ded7b86efc3d51a8
RayyesAbed <abedray1567@gmail.com>
2024-12-27 20:32:26 +0100

    Added the register path and Component to App Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Introduction from "./pages/introduction/Introduction";
 import { BrowserRouter, Routes, Route } from "react-router";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index path="/" element={<Introduction />} />
+        <Route index path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
