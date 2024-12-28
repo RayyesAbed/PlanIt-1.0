6050aa11de3ef781a7fdca3a8b62a5fdb5e29a08
RayyesAbed <abedray1567@gmail.com>
2024-12-28 13:51:38 +0100

    Added the Login component to the login path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import Introduction from "./pages/introduction/Introduction";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index path="/" element={<Introduction />} />
-        <Route index path="/register" element={<Register />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
