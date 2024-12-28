80e9d0e6a424546ffea72a4ba7fdab58b9cb6522
RayyesAbed <abedray1567@gmail.com>
2024-12-28 13:50:05 +0100

    Added the first section which is an image

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,5 +1,15 @@
 import styles from "./Login.module.css";
+import LoginSectionImg from "/LoginSectionImg.webp";
 
-const Login = () => {};
+const Login = () => {
+  return (
+    <div className={styles.loginDiv}>
+      <section>
+        <img src={LoginSectionImg} alt="Login Image" />
+      </section>
+      <section></section>
+    </div>
+  );
+};
 
 export default Login;
