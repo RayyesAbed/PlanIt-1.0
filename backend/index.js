46fcd48b62e046ec643be47e1e0b88e60b89eb85
RayyesAbed <abedray1567@gmail.com>
2024-12-29 13:46:17 +0100

    Worked with mongoose ODM and dotenv for loading env files:
    - Loaded (for now development env) into index js file
    - Connected to mongoDB with mongoose and added both success and failure with .then and .catch

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,20 @@
+// require all modules here
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "../frontend/.env.development" });
 
 const app = express();
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_NAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.MONGODB_APPNAME}`
+  )
+  .then(() => {
+    console.log("CONNECTED");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
