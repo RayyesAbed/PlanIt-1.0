5acbb12d163964c8b0b0e275e1ded57b74a46a11
RayyesAbed <abedray1567@gmail.com>
2024-12-30 18:41:34 +0100

    Returned a basic nav element and added className attribute to it

--- frontend/src/components/userPageMenu/UserPageMenu.jsx
+++ frontend/src/components/userPageMenu/UserPageMenu.jsx
@@ -1,5 +1,7 @@
 import styles from "./userPageMenu.module.css";
 
-const UserPageMenu = () => {};
+const UserPageMenu = () => {
+  return <nav className={styles.menuNav}></nav>;
+};
 
 export default UserPageMenu;
