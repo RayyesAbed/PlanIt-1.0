fe28d6a643123bf4abc31771fd561b6a2f18e804
RayyesAbed <abedray1567@gmail.com>
2024-12-30 19:46:39 +0100

    Returned the account wrapper with its dummy data for now

--- frontend/src/components/userPageMenu/UserPageMenu.jsx
+++ frontend/src/components/userPageMenu/UserPageMenu.jsx
@@ -1,7 +1,18 @@
 import styles from "./userPageMenu.module.css";
+import TestImg from "/AbdallahImg.jpg"; // only for testing purposes
 
 const UserPageMenu = () => {
-  return <nav className={styles.menuNav}></nav>;
+  return (
+    <nav className={styles.menuNav}>
+      <div className={styles.accountWrapper}>
+        <img src={TestImg} alt="Your profile photo" />
+        <div className={styles.userNameAndPlanWrapper}>
+          <p>Abdallah Alrayyes</p> {/* only for testing purposes */}
+          <p>Free Plan</p> {/* only for testing purposes */}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default UserPageMenu;
