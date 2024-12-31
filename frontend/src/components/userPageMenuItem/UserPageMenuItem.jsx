70df9f552f49dd2ae2fd73a17cc00ccdb753fdd4
RayyesAbed <abedray1567@gmail.com>
2024-12-31 13:01:38 +0100

    Returned a div with an icon div wrapper and the heading 4, used the className attribute for styling purposes

--- frontend/src/components/userPageMenuItem/UserPageMenuItem.jsx
+++ frontend/src/components/userPageMenuItem/UserPageMenuItem.jsx
@@ -1,5 +1,12 @@
 import styles from "./UserPageMenuItem.module.css";
 
-const UserPageMenuItem = () => {};
+const UserPageMenuItem = ({ iconImg, menuItemText }) => {
+  return (
+    <div className={styles.userPageMenuItem}>
+      <div>{iconImg}</div>
+      <h4>{menuItemText}</h4>
+    </div>
+  );
+};
 
 export default UserPageMenuItem;
