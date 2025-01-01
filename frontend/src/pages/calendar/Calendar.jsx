ea18023626c4c7d8ae306853de77f70b41a78993
RayyesAbed <abedray1567@gmail.com>
2025-01-01 14:13:18 +0100

    Removed the Localization from Calendar component, to be applied in main jsx file for better reach across all components

--- frontend/src/pages/calendar/Calendar.jsx
+++ frontend/src/pages/calendar/Calendar.jsx
@@ -1,17 +1,12 @@
 import UserPageMenu from "../../components/userPageMenu/UserPageMenu";
 import styles from "./Calendar.module.css";
-import dayjs from "dayjs";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
 const Calendar = () => {
   return (
     <div className={styles.calendarWrapper}>
       <UserPageMenu />
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateCalendar className={styles.dateCalendar} />
-      </LocalizationProvider>
+      <DateCalendar className={styles.dateCalendar} />
     </div>
   );
 };
