cc37772d7d6a0381cf4dfe10ed8ce6e8a1ae486d
RayyesAbed <abedray1567@gmail.com>
2025-01-01 13:29:54 +0100

    Added basic DateCalendar and wrapped it with Localization Provider

--- frontend/src/pages/calendar/Calendar.jsx
+++ frontend/src/pages/calendar/Calendar.jsx
@@ -1,10 +1,17 @@
 import UserPageMenu from "../../components/userPageMenu/UserPageMenu";
 import styles from "./Calendar.module.css";
+import dayjs from "dayjs";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
 const Calendar = () => {
   return (
     <div className={styles.calendarWrapper}>
       <UserPageMenu />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateCalendar className={styles.dateCalendar} />
+      </LocalizationProvider>
     </div>
   );
 };
