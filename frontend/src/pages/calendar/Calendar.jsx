fada93cb22572c5b5ebea0f49e3c2dea4db366f9
RayyesAbed <abedray1567@gmail.com>
2025-01-01 12:23:37 +0100

    Returned my built-in UserPageMenu component wrapped in a calendar wrapper

--- frontend/src/pages/calendar/Calendar.jsx
+++ frontend/src/pages/calendar/Calendar.jsx
@@ -1,5 +1,12 @@
+import UserPageMenu from "../../components/userPageMenu/UserPageMenu";
 import styles from "./Calendar.module.css";
 
-const Calendar = () => {};
+const Calendar = () => {
+  return (
+    <div className={styles.calendarWrapper}>
+      <UserPageMenu />
+    </div>
+  );
+};
 
 export default Calendar;
