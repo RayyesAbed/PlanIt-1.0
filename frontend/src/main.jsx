b795f34489748900c70ecef3ca8eef8a46345e9c
RayyesAbed <abedray1567@gmail.com>
2025-01-02 05:19:37 +0100

    Wrapped my Application with Mantine Provider Component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,14 @@ import App from "./App.jsx";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs from "dayjs";
+import { MantineProvider } from "@mantine/core";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <App />
-    </LocalizationProvider>
+    <MantineProvider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <App />
+      </LocalizationProvider>
+    </MantineProvider>
   </StrictMode>
 );
