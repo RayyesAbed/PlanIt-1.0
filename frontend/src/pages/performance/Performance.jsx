143968f3eee0aad5b52831f727220b8d3406937a
RayyesAbed <abedray1567@gmail.com>
2025-01-05 14:04:11 +0100

    Used Chart Component and added dummy data to it, styled it with height and width, and added options with the title and the curve to it

--- frontend/src/pages/performance/Performance.jsx
+++ frontend/src/pages/performance/Performance.jsx
@@ -2,11 +2,27 @@ import UserPageMenu from "../../components/forExistingUsers/userPageMenu/UserPag
 import styles from "./Performance.module.css";
 import { Chart } from "react-google-charts";
 
+// some dummy data, to be replaced later by the backend fetches
+const data = [
+  ["x", "dogs"],
+  [0, 0],
+  [1, 10],
+  [2, 23],
+];
+
 const Performance = () => {
   return (
     <div className={styles.wrapper}>
       <UserPageMenu />
-      <div className={styles.chartWrapper}></div>
+      <div className={styles.chartWrapper}>
+        <Chart
+          data={data}
+          chartType="LineChart"
+          height="600px"
+          width="100%"
+          options={{ title: "My Performance", curveType: "function" }}
+        />
+      </div>
     </div>
   );
 };
