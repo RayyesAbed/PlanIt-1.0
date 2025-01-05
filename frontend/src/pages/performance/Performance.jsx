8a82faa3d6dd20fc9980851d30151311387e213b
RayyesAbed <abedray1567@gmail.com>
2025-01-05 13:06:21 +0100

    Returned basic JSX and used the className and applied styling in Performance CSS module file

--- frontend/src/pages/performance/Performance.jsx
+++ frontend/src/pages/performance/Performance.jsx
@@ -1,5 +1,14 @@
+import UserPageMenu from "../../components/forExistingUsers/userPageMenu/UserPageMenu";
 import styles from "./Performance.module.css";
+import { LineChart } from "@mantine/charts";
 
-const Performance = () => {};
+const Performance = () => {
+  return (
+    <div className={styles.wrapper}>
+      <UserPageMenu />
+      <div className={styles.chartWrapper}></div>
+    </div>
+  );
+};
 
 export default Performance;
