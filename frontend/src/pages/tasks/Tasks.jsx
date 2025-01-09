b0398fdeb7152d5d7b56e77da430db7c93998da0
RayyesAbed <abedray1567@gmail.com>
2025-01-09 19:22:31 +0100

    Returned the first div which is contains (Tasks h1, the tasks search input, and an icon for opening a dialog)

--- frontend/src/pages/tasks/Tasks.jsx
+++ frontend/src/pages/tasks/Tasks.jsx
@@ -1,4 +1,6 @@
 import UserPageMenu from "../../components/forExistingUsers/userPageMenu/UserPageMenu";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import Tooltip from "@mui/material/Tooltip";
 
 import styles from "./Tasks.module.css";
 
@@ -6,7 +8,15 @@ const Tasks = () => {
   return (
     <div className={styles.wrapper}>
       <UserPageMenu />
-      <div className={styles.tasksWrapper}></div>
+      <div className={styles.tasksWrapper}>
+        <div>
+          <h1>Tasks</h1>
+          <input type="text" name="taskSearch" placeholder="Search Tasks..." />
+          <Tooltip title="Add Task">
+            <AddCircleIcon className={styles.addTaskDialogIcon} />
+          </Tooltip>
+        </div>
+      </div>
     </div>
   );
 };
