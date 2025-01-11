8a317a317e8fd97f08b17d93a683d4823427d381
RayyesAbed <abedray1567@gmail.com>
2025-01-11 16:32:45 +0100

    Used AddTaskDialog component in Tasks, and used its props for handling showing and closing dialog

--- frontend/src/components/application/tasks/Tasks.jsx
+++ frontend/src/components/application/tasks/Tasks.jsx
@@ -3,8 +3,16 @@ import AddCircleIcon from "@mui/icons-material/AddCircle";
 import Tooltip from "@mui/material/Tooltip";
 
 import styles from "./Tasks.module.css";
+import AddTaskDialog from "../common/dialogs/addTask/AddTaskDialog";
+import { useState } from "react";
 
 const Tasks = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleShowDialog = () => setIsDialogOpen(true);
+
+  const handleCloseDialog = () => setIsDialogOpen(false);
+
   return (
     <div className={styles.wrapper}>
       <UserPageMenu />
@@ -12,11 +20,12 @@ const Tasks = () => {
         <div>
           <h1>Tasks</h1>
           <input type="text" name="taskSearch" placeholder="Search Tasks..." />
-          <Tooltip title="Add Task">
+          <Tooltip title="Add Task" onClick={handleShowDialog}>
             <AddCircleIcon className={styles.addTaskDialogIcon} />
           </Tooltip>
         </div>
       </div>
+      <AddTaskDialog openModal={isDialogOpen} closeModal={handleCloseDialog} />
     </div>
   );
 };
