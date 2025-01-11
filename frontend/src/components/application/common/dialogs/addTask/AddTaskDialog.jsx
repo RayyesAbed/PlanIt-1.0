e1f610fdfaa672e0369610a8e1cff33813a7da04
RayyesAbed <abedray1567@gmail.com>
2025-01-11 16:31:13 +0100

    Handled viewing and hiding the Add Task Dialog component with useRef and useEffect

--- frontend/src/components/application/common/dialogs/addTask/AddTaskDialog.jsx
+++ frontend/src/components/application/common/dialogs/addTask/AddTaskDialog.jsx
@@ -1,5 +1,18 @@
+import { useEffect, useRef } from "react";
 import styles from "./AddTaskDialog.module.css";
 
-const AddTaskDialog = () => {};
+const AddTaskDialog = ({ openModal, closeModal }) => {
+  const ref = useRef(); // modal ref
+
+  useEffect(() => {
+    if (openModal) {
+      ref.current?.showModal();
+    } else {
+      ref.current?.close();
+    }
+  });
+
+  return <dialog ref={ref} onCancel={closeModal}></dialog>;
+};
 
 export default AddTaskDialog;
