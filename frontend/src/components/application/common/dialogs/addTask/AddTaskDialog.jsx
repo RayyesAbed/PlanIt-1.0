a1f819e544cdbdedde898418c02ac96c538842d1
RayyesAbed <abedray1567@gmail.com>
2025-01-11 16:38:51 +0100

    Added animation with React motion to the AddTaskDialog when showing the dialog

--- frontend/src/components/application/common/dialogs/addTask/AddTaskDialog.jsx
+++ frontend/src/components/application/common/dialogs/addTask/AddTaskDialog.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useRef } from "react";
+import * as motion from "motion/react-client";
+import { AnimatePresence } from "motion/react";
 import styles from "./AddTaskDialog.module.css";
 
 const AddTaskDialog = ({ openModal, closeModal }) => {
@@ -12,7 +14,19 @@ const AddTaskDialog = ({ openModal, closeModal }) => {
     }
   });
 
-  return <dialog ref={ref} onCancel={closeModal}></dialog>;
+  return (
+    <AnimatePresence initial={false}>
+      {openModal && (
+        <motion.dialog
+          ref={ref}
+          onCancel={closeModal}
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0 }}
+        ></motion.dialog>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default AddTaskDialog;
