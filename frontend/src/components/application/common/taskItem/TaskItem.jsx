220b2d19687e79e48377909e5ca43a438b1b8d38
RayyesAbed <abedray1567@gmail.com>
2025-01-20 00:17:19 +0100

    Improved returned JSX code, added dummy bonus text (To be replaced later)m added a Checkbox, and added classNames

--- frontend/src/components/application/common/taskItem/TaskItem.jsx
+++ frontend/src/components/application/common/taskItem/TaskItem.jsx
@@ -1,15 +1,15 @@
 import styles from "./TaskItem.module.css";
+import Checkbox from "@mui/material/Checkbox";
 
 const TaskItem = ({ taskName, taskDueDate, taskPriority, taskDescription }) => {
   return (
-    <div>
-      <img />
-      <div>
-        <h2>{taskName}</h2>
-        <h2>{taskDueDate}</h2>
-        <h2>{taskPriority}</h2>
-      </div>
-      <h3>{taskDescription}</h3>
+    <div className={styles.taskItem}>
+      <Checkbox className={styles.tasksItemChecbox} />
+      <h2>{taskName}</h2>
+      <h2>{taskDueDate}</h2>
+      <h2 className={styles[taskPriority]}>{taskPriority}</h2>
+      <h2>+35</h2>
+      <p>{taskDescription}</p>
     </div>
   );
 };
