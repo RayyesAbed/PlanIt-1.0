9995a76fe88d022b108f1b83fe2330cfe3b0fb8d
RayyesAbed <abedray1567@gmail.com>
2025-01-19 18:46:22 +0100

    Passed props to TaskItem and returned a very basic JSX, styling to be added later

--- frontend/src/components/application/common/taskItem/TaskItem.jsx
+++ frontend/src/components/application/common/taskItem/TaskItem.jsx
@@ -1,5 +1,17 @@
 import styles from "./TaskItem.module.css";
 
-const TaskItem = () => {};
+const TaskItem = ({ taskName, taskDueDate, taskPriority, taskDescription }) => {
+  return (
+    <div>
+      <img />
+      <div>
+        <h2>{taskName}</h2>
+        <h2>{taskDueDate}</h2>
+        <h2>{taskPriority}</h2>
+      </div>
+      <h3>{taskDescription}</h3>
+    </div>
+  );
+};
 
 export default TaskItem;
