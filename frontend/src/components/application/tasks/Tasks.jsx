fbcd6de0bf83c5fce83d66718d4e9871173d1dbd
RayyesAbed <abedray1567@gmail.com>
2025-01-19 18:47:22 +0100

    Mapped state (An array of tasks) to each task item with its properties

--- frontend/src/components/application/tasks/Tasks.jsx
+++ frontend/src/components/application/tasks/Tasks.jsx
@@ -6,10 +6,11 @@ import styles from "./Tasks.module.css";
 import AddTaskDialog from "../common/dialogs/addTask/AddTaskDialog";
 import { useContext, useState } from "react";
 import { TaskContext } from "../../../contexts/TaskContext";
+import TaskItem from "../common/taskItem/TaskItem";
 
 const Tasks = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const { state } = useContext(TaskContext);
+  const { state } = useContext(TaskContext); // state includes the array of tasks
 
   const handleShowDialog = () => setIsDialogOpen(true);
 
@@ -25,6 +26,17 @@ const Tasks = () => {
           <Tooltip title="Add Task" onClick={handleShowDialog}>
             <AddCircleIcon className={styles.addTaskDialogIcon} />
           </Tooltip>
+        </div>
+        <div>
+          {state.map((taskItem) => (
+            <TaskItem
+              key={taskItem.id}
+              taskName={taskItem.taskName}
+              taskDueDate={taskItem.taskDueDate}
+              taskPriority={taskItem.taskPriority}
+              taskDescription={taskItem.taskDescription}
+            />
+          ))}
         </div>
       </div>
       <AddTaskDialog openModal={isDialogOpen} closeModal={handleCloseDialog} />
