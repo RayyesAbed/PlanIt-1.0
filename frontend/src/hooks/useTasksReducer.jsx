513ddbb8c43ce47445581de591f214ed8649914f
RayyesAbed <abedray1567@gmail.com>
2025-01-19 18:23:26 +0100

    Improved the logic of the custom reducer hook by using only array

--- frontend/src/hooks/useTasksReducer.jsx
+++ frontend/src/hooks/useTasksReducer.jsx
@@ -1,19 +1,17 @@
-import { useContext, useReducer } from "react";
-import TaskContext from "../contexts/TaskContext";
+import { useReducer } from "react";
+import { tasks } from "../contexts/TaskContext";
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD":
-      return { ...state, state: [...state.state, action.payload] }; // Grab the existing dispatch and modify the state which includes tasks array
+      return [...state, action.payload]; // Grab the existing dispatch and modify the state which includes tasks array
     default:
       return state;
   }
 };
 
 const useTasksReducer = () => {
-  const taskContext = useContext(TaskContext);
-
-  return useReducer(reducer, taskContext);
+  return useReducer(reducer, tasks);
 };
 
 export default useTasksReducer;
