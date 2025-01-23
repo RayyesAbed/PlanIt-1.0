218fabf1f0545969bac67612bd4140236549d5db
RayyesAbed <abedray1567@gmail.com>
2025-01-23 20:32:07 +0100

    Used the EDIT action type to return the newly edited task item with the rest of the tasks

--- frontend/src/hooks/useTasksReducer.jsx
+++ frontend/src/hooks/useTasksReducer.jsx
@@ -7,6 +7,11 @@ const reducer = (state, action) => {
       return [...state, action.payload]; // Grab the existing dispatch and modify the state which includes tasks array
     case "DELETE":
       return [...state.filter((task) => task.id !== action.payload)]; // filter based on the task id and return the new state without the deleted task
+    case "EDIT":
+      return [
+        action.payload,
+        ...state.filter((task) => task.id !== action.payload.id),
+      ]; // return the payload and the rest of the tasks
     default:
       return state;
   }
