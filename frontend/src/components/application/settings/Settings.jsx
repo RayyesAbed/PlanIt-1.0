93a9144ebdc1ee709c6f1f3eca7287045582d78e
RayyesAbed <abedray1567@gmail.com>
2025-01-24 12:25:23 +0100

    Returned the wrapper with the UserPageMenu inside it and gave the wrapper a className

--- frontend/src/components/application/settings/Settings.jsx
+++ frontend/src/components/application/settings/Settings.jsx
@@ -1,5 +1,12 @@
+import UserPageMenu from "../common/userPageMenu/UserPageMenu";
 import styles from "./Settings.module.css";
 
-const Settings = () => {};
+const Settings = () => {
+  return (
+    <div className={styles.wrapper}>
+      <UserPageMenu />
+    </div>
+  );
+};
 
 export default Settings;
