f6a08991216d43632c186d83ced3edfd79e26923
RayyesAbed <abedray1567@gmail.com>
2025-01-28 20:22:47 +0100

    Checked whether a user exists based on their email value

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,7 @@
 const isPasswordValid = require("../utils/isPasswordValid");
 const isThereSpecialCharacters = require("../utils/isThereSpecialCharacters");
 const isValidEmail = require("../utils/isValidEmail");
+const User = require("../schemas/User");
 
 const registerUser = (req, res) => {
   const { name, email, password } = req.body;
@@ -25,6 +26,13 @@ const registerUser = (req, res) => {
   // check if password is at least 8 characters long and is secure enough
   if (!isPasswordValid(password)) {
     return res.status(400).json({ message: "Password is not secure enough!" });
+  }
+
+  let user = User.findOne({ email: email });
+  if (user) {
+    return res
+      .status(400)
+      .json({ message: "A user with that email already exists!" });
   }
 };
 
