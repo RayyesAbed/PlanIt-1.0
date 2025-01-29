7ca7620ec77aabc3a092f73d1982d3505e2a5058
RayyesAbed <abedray1567@gmail.com>
2025-01-29 06:39:15 +0100

    Generated an email token, created a validation URL, sent the email, and returned a status 200 and a message upon successful validation email sending

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,8 @@
 const isPasswordValid = require("../utils/isPasswordValid");
 const isThereSpecialCharacters = require("../utils/isThereSpecialCharacters");
 const isValidEmail = require("../utils/isValidEmail");
+const generateToken = require("../services/generateToken");
+const sendEmail = require("../services/sendEmail");
 const User = require("../schemas/User");
 
 const registerUser = async (req, res) => {
@@ -34,6 +36,30 @@ const registerUser = async (req, res) => {
       .status(400)
       .json({ message: "A user with that email already exists!" });
   }
+
+  const emailToken = generateToken(email);
+  const validationURL = `${process.env.VITE_FRONTEND_URL}/validate-email?token=${emailToken}`;
+
+  await sendEmail(
+    email,
+    "PlanIt Email Validation",
+    `
+    <p> Hi ${name}, </p>
+
+    <p> Are you ready to start your journey with PlanIt? then please click on the verification link: ${validationURL}. </p>
+
+    <p>This link expires in an hour. </p>
+
+    <p>Not you. Then please feel free to ignore this email. </p>
+
+    <p>Thanks</p>
+    <p>PlanIt</p>
+    `
+  );
+
+  return res
+    .status(200)
+    .json({ message: "A validation email was sent successfully" });
 };
 
 module.exports = {
