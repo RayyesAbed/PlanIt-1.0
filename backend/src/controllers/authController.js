e3ece9fbad8c7041a56041681b9041e53d98a234
RayyesAbed <abedray1567@gmail.com>
2025-01-29 08:47:58 +0100

    Used the hash password function, used pending users for temporary storage, and created a verify email function where I verified the token

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -4,6 +4,9 @@ const isValidEmail = require("../utils/isValidEmail");
 const generateToken = require("../services/generateToken");
 const sendEmail = require("../services/sendEmail");
 const User = require("../schemas/User");
+const PendingUser = require("../schemas/PendingUser");
+const verifyToken = require("../services/verifyToken");
+const hashPassword = require("../services/hashPassword");
 
 const registerUserRequest = async (req, res) => {
   const { name, email, password } = req.body;
@@ -37,8 +40,17 @@ const registerUserRequest = async (req, res) => {
       .json({ message: "A user with that email already exists!" });
   }
 
+  const hashedPassword = hashPassword(password);
+
+  // Only for passing user's data to verify email function
+  await PendingUser.create({
+    name: name,
+    email: email,
+    password: hashedPassword,
+  });
+
   const emailToken = generateToken(email);
-  const validationURL = `${process.env.VITE_FRONTEND_URL}/validate-email?token=${emailToken}`;
+  const validationURL = `${process.env.VITE_FRONTEND_URL}/verify-email?token=${emailToken}`;
 
   await sendEmail(
     email,
@@ -62,6 +74,30 @@ const registerUserRequest = async (req, res) => {
     .json({ message: "A validation email was sent successfully" });
 };
 
+const verifyEmail = async (req, res) => {
+  const { token } = req.query;
+
+  try {
+    const { email } = verifyToken(token);
+    const pendingUser = await PendingUser.findOne({ email: email });
+
+    await User.create({
+      name: pendingUser.name,
+      email: pendingUser.email,
+      password: pendingUser.password,
+    });
+
+    PendingUser.deleteOne({ email: email });
+
+    return res
+      .status(200)
+      .json({ message: "The user was created successfully!" });
+  } catch (error) {
+    return res.status(400).json({ message: "Invalid or expired token!" });
+  }
+};
+
 module.exports = {
   registerUserRequest,
+  verifyEmail,
 };
