3a724badf4d390cb5f26fe4af3b2a1131a6b1fc5
RayyesAbed <abedray1567@gmail.com>
2025-02-05 06:01:30 +0100

    Used my loginUser function and added it to the /login middleware, along with checking with Express Validator

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const { check } = require("express-validator");
 const router = express.Router();
 
 const {
   registerUserRequest,
   verifyEmail,
+  loginUser,
 } = require("../controllers/authController");
 
 router.post("/register_request", registerUserRequest);
 router.post("/verify-email", verifyEmail);
+router.post(
+  "/login",
+  [
+    check("email").isEmail().withMessage("Invalid email format"),
+    check("password").notEmpty().withMessage("Password is required"),
+  ],
+  loginUser
+);
 
 module.exports = router;
