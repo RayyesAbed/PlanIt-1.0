48b21cd6f64f0174c81559f492f3c870a70ca85e
RayyesAbed <abedray1567@gmail.com>
2025-02-06 18:50:30 +0100

    Added configuration to CORS (included credentials, and origin (Frontend))

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const authRoutes = require("./src/routes/authRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: process.env.VITE_FRONTEND_URL, credentials: true }));
 
 app.use(express.json());
 
