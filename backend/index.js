31c8511a0f73f5e9a1cb80de3b8f50d37db60026
RayyesAbed <abedray1567@gmail.com>
2025-02-07 09:53:44 +0100

    Used the cookie parser in my app use middleware in index JS file

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,15 @@ const express = require("express");
 const cors = require("cors");
 const mongooseConnect = require("./src/configs/mongooseConnect");
 const authRoutes = require("./src/routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 app.use(cors({ origin: process.env.VITE_FRONTEND_URL, credentials: true }));
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/auth", authRoutes);
 
