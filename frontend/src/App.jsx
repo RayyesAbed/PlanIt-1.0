4a8fbe245429f2919b324f9e0f65bbc9ded301f2
RayyesAbed <abedray1567@gmail.com>
2025-02-07 12:31:24 +0100

    Added my Protected Route to App JSX component (for protected routes) and handled /login route with my useCheckAuth hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import Introduction from "./components/introduction/introductionPage/Introduction";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import Register from "./components/authentication/register/Register";
 import Login from "./components/authentication/login/Login";
 import UserPage from "./components/application/userPage/userPage";
@@ -9,20 +9,30 @@ import Tasks from "./components/application/tasks/Tasks";
 import "./App.css";
 import Settings from "./components/application/settings/Settings";
 import EmailVerify from "./components/authentication/verify/EmailVerify";
+import ProtectedRoute from "./components/authentication/protected/ProtectedRoute";
+import useCheckAuth from "./hooks/useCheckAuth";
 
 function App() {
+  const isAuthenticated = useCheckAuth();
+
   return (
     <BrowserRouter>
       <Routes>
         <Route index path="/" element={<Introduction />} />
         <Route path="/register" element={<Register />} />
         <Route path="/verify-email" element={<EmailVerify />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<UserPage />} />
-        <Route path="/calendar" element={<CalendarComponent />} />
-        <Route path="/performance" element={<Performance />} />
-        <Route path="/tasks" element={<Tasks />} />
-        <Route path="/settings/*" element={<Settings />} />
+        <Route
+          path="/login"
+          element={isAuthenticated ? <Navigate to="/home" /> : <Login />}
+        />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/home" element={<UserPage />} />
+          <Route path="/calendar" element={<CalendarComponent />} />
+          <Route path="/performance" element={<Performance />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/settings/*" element={<Settings />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
