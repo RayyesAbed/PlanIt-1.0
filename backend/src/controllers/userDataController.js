04a540cbbc37af8614f2682ed528ed7170a981da
RayyesAbed <abedray1567@gmail.com>
2025-02-14 09:48:39 +0100

    Created a controller for getting user data:
    - Checked the existence of cookies and then tokens
    - extracted userId from token upon successful verification
    - Checked the existence of user
    - Returned upon successful user data fetch
    - And catch if there are errors

---
+++ backend/src/controllers/userDataController.js
@@ -0,0 +1,37 @@
+const verifyToken = require("../services/verifyToken");
+const User = require("../schemas/User");
+
+const getUserData = async (req, res, next) => {
+  try {
+    // if no cookie is set
+    if (!req.headers.cookie) {
+      return res.sendStatus(401);
+    }
+
+    const token = req.headers.cookie.split("=")[1];
+
+    // if there is no token
+    if (!token) {
+      return res.sendStatus(401);
+    }
+
+    const { userId } = verifyToken(token);
+
+    const user = await User.findById(userId);
+
+    // if user was not found
+    if (!user) {
+      return res.status(404).json({ message: "User does not exist." });
+    }
+
+    return res.status(200).json({
+      name: user.name,
+      email: user.email,
+    });
+  } catch (error) {
+    console.error("Error while getting user data", error);
+    return res.sendStatus(500);
+  }
+};
+
+module.exports = getUserData;
