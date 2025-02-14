a6fe6234749418b855cf38918f6cdfeef9c4df9e
RayyesAbed <abedray1567@gmail.com>
2025-02-14 09:54:37 +0100

    Created a userDataRoutes file to pass the controller into the middleware

---
+++ backend/src/routes/userDataRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const getUserData = require("../controllers/userDataController");
+const router = express.Router();
+
+router.get("/", getUserData);
+
+module.exports = router;
