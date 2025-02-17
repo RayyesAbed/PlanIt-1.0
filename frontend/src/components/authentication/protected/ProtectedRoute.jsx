b81ca7a48b3416d47fc2074f0b6dd1abe4c3e8b6
RayyesAbed <abedray1567@gmail.com>
2025-02-17 15:10:27 +0100

    Wrapped my outlet with the task provider which is lazily loaded

--- frontend/src/components/authentication/protected/ProtectedRoute.jsx
+++ frontend/src/components/authentication/protected/ProtectedRoute.jsx
@@ -1,4 +1,6 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { lazy } from "react";
+const TaskProvider = lazy(() => import("../../../providers/TaskProvider"));
 import useCheckAuth from "../../../hooks/useCheckAuth";
 
 const ProtectedRoute = () => {
@@ -8,7 +10,13 @@ const ProtectedRoute = () => {
     return <p>Loading...</p>; // to be changed later
   }
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+  return isAuthenticated ? (
+    <TaskProvider>
+      <Outlet />
+    </TaskProvider>
+  ) : (
+    <Navigate to="/login" />
+  );
 };
 
 export default ProtectedRoute;
