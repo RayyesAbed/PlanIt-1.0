07156f774a62add26fe8ee7c55aafebd540e57b9
RayyesAbed <abedray1567@gmail.com>
2025-02-17 13:44:18 +0100

    Removed outsourced token generation function and generated the token in authController

--- backend/src/services/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (userId, type) => {
-  return jwt.sign({ userId, type }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
-  });
-};
-
-module.exports = generateToken;
