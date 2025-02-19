17120c0092de54cf4dde35ac26e8b47ba1bf575a
RayyesAbed <abedray1567@gmail.com>
2025-02-19 17:40:28 +0100

    Created a function in the backend for editing a particular task

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -48,7 +48,44 @@ const addTask = async (req, res) => {
   }
 };
 
+const editTask = async (req, res) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) return res.sendStatus(401);
+
+    let decoded;
+
+    try {
+      decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+      return res.sendStatus(403);
+    }
+
+    const updatedTaskData = req.body;
+
+    const userTasks = await Task.findOne({ userID: decoded.userId });
+
+    const taskIndex = userTasks.list.findIndex(
+      (task) => task.id === updatedTaskData.id
+    );
+
+    if (taskIndex === -1)
+      return res.status(404).json({ message: "Task not found" });
+
+    userTasks.list[taskIndex] = updatedTaskData;
+
+    await userTasks.save();
+
+    return res.status(200).json({ message: "Task updated successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   getTasks,
   addTask,
+  editTask,
 };
