a89f82db546edd3c55f7f501bd252a24482b313d
RayyesAbed <abedray1567@gmail.com>
2025-02-19 17:41:17 +0100

    Added the task edit function into its own middleware with the /edit_task API endpoint, and used a PUT method

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { getTasks, addTask } = require("../controllers/taskController");
+const {
+  getTasks,
+  addTask,
+  editTask,
+} = require("../controllers/taskController");
 
 const router = express.Router();
 
 router.get("/list", getTasks);
 
 router.post("/add_task", addTask);
+
+router.put("/edit_task", editTask);
 
 module.exports = router;
