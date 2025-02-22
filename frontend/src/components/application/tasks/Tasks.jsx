00beffe6f5bfa5cd239afb62dc6c6cf426e2918d
RayyesAbed <abedray1567@gmail.com>
2025-02-22 19:03:28 +0100

    Created a function for handling completed tasks where I disptached to the reducer and sent the request with the task ID to the backend

--- frontend/src/components/application/tasks/Tasks.jsx
+++ frontend/src/components/application/tasks/Tasks.jsx
@@ -9,6 +9,7 @@ import { TaskContext } from "../../../contexts/TaskContext";
 import TaskItem from "../common/taskItem/TaskItem";
 import EditTaskDialog from "../common/dialogs/editTask/EditTaskDialog";
 import { deleteTask } from "../../../api/deleteTask";
+import { completeTask } from "../../../api/completeTask";
 
 const Tasks = () => {
   const [isAddDialogOpen, setIsAddDialogOpen] = useState(false);
@@ -34,6 +35,11 @@ const Tasks = () => {
     setFilteredTask(task);
   };
 
+  const handleCompleteTask = async (taskId) => {
+    dispatch({ type: "COMPLETE", payload: taskId });
+    await completeTask(taskId);
+  };
+
   return (
     <div className={styles.wrapper}>
       <UserPageMenu />
@@ -56,6 +62,7 @@ const Tasks = () => {
                 taskDescription={taskItem.taskDescription}
                 bonusPoints={taskItem.bonusPoints}
                 completed={taskItem.completed}
+                onTaskComplete={() => handleCompleteTask(taskItem.id)}
                 onTaskDelete={() => handleDeleteTask(taskItem.id)}
                 onTaskEdit={() => handleEditTask(taskItem)}
               />
