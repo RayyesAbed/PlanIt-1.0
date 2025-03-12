a684237eb4e7a1e113f4d90caf9d05da74d639b5
RayyesAbed <abedray1567@gmail.com>
2025-03-12 19:07:18 +0100

    Changed the name of the files from User Page to Store

--- frontend/src/components/application/store/Store.jsx
+++ frontend/src/components/application/store/Store.jsx
@@ -1,7 +1,7 @@
 import UserPageMenu from "../common/userPageMenu/UserPageMenu";
-import styles from "./UserPage.module.css";
+import styles from "./Store.module.css";
 
-const UserPage = () => {
+const Store = () => {
   return (
     <div className={styles.userPageWrapper}>
       <UserPageMenu />
@@ -9,4 +9,4 @@ const UserPage = () => {
   );
 };
 
-export default UserPage;
+export default Store;
