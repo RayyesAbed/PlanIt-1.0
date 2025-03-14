bc41653ec398c64a42477cf0b8d20adfd4759c56
RayyesAbed <abedray1567@gmail.com>
2025-03-14 21:58:04 +0100

    Improved the Chart design by styling the V axis, H axis, as well as the title text style, and simulated the dummy data (To be replaced later by the data fetched from backend)

--- frontend/src/components/application/performance/Performance.jsx
+++ frontend/src/components/application/performance/Performance.jsx
@@ -4,13 +4,17 @@ import { Chart } from "react-google-charts";
 
 // some dummy data, to be replaced later by the backend fetches
 const data = [
-  ["x", "dogs"],
-  [0, 0],
-  [1, 10],
-  [2, 23],
+  ["x", "tasks completed per day"],
+  ["03-14", 2],
+  ["03-17", 3],
+  ["03-18", 1],
+  ["03-20", 7],
+  ["03-23", 5],
 ];
 
 const Performance = () => {
+  const textStyle = { color: "white" };
+
   return (
     <div className={styles.wrapper}>
       <UserPageMenu />
@@ -20,7 +24,28 @@ const Performance = () => {
           chartType="LineChart"
           height="600px"
           width="100%"
-          options={{ title: "My Performance", curveType: "function" }}
+          options={{
+            title: "My Performance",
+            titleTextStyle: {
+              ...textStyle,
+            },
+            curveType: "function",
+            backgroundColor: "black",
+            vAxis: {
+              title: "Number of Tasks completed",
+              titleTextStyle: textStyle,
+              textStyle,
+            },
+            hAxis: {
+              title: "Date",
+              format: "MM-DD",
+              titleTextStyle: textStyle,
+              textStyle,
+            },
+            legend: {
+              textStyle,
+            },
+          }}
         />
       </div>
     </div>
