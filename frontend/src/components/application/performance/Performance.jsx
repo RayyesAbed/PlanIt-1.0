fe21cb46ccdb78d4109f211a2a0b579594793498
RayyesAbed <abedray1567@gmail.com>
2025-03-17 19:49:04 +0100

    Handled the completed tasks' days array and added comments for more clarity, and remove the curve design on chart

--- frontend/src/components/application/performance/Performance.jsx
+++ frontend/src/components/application/performance/Performance.jsx
@@ -1,26 +1,46 @@
 import UserPageMenu from "../common/userPageMenu/UserPageMenu";
 import styles from "./Performance.module.css";
 import { Chart } from "react-google-charts";
-
-// some dummy data, to be replaced later by the backend fetches
-const data = [
-  ["x", "tasks completed per day"],
-  ["03-14", 2],
-  ["03-17", 3],
-  ["03-18", 1],
-  ["03-20", 7],
-  ["03-23", 5],
-];
+import dayjs from "dayjs";
+import { useContext, useMemo } from "react";
+import { TaskContext } from "../../../contexts/TaskContext";
 
 const Performance = () => {
   const textStyle = { color: "white" };
+  const { state } = useContext(TaskContext);
+
+  const completedTasksDays = useMemo(() => {
+    // initialize a counter
+    let counter = 0;
+
+    // create a days array and initialize the completed tasks to zeros
+    const days = Array(7)
+      .fill()
+      .map((_, day) => [dayjs().subtract(day, "day").format("MM-DD"), 0]);
+
+    // modify the completed tasks on days array based on number of completed tasks by looping through days and state (fetched tasks)
+    days.forEach((tasksDayItem) => {
+      state.forEach((task) => {
+        if (
+          task.completed && // check first if task is completed
+          dayjs(task.taskDueDate).format("MM-DD") === tasksDayItem[0] // then check if task due date is equal to the day item
+        ) {
+          counter++;
+        }
+      });
+      tasksDayItem[1] = counter; // Save the counter value in completed tasks in days
+      counter = 0; // reset the counter
+    });
+
+    return [["Day", "Tasks Completed"], ...days];
+  }, [state]);
 
   return (
     <div className={styles.wrapper}>
       <UserPageMenu />
       <div className={styles.chartWrapper}>
         <Chart
-          data={data}
+          data={completedTasksDays}
           chartType="LineChart"
           height="600px"
           width="100%"
@@ -29,7 +49,6 @@ const Performance = () => {
             titleTextStyle: {
               ...textStyle,
             },
-            curveType: "function",
             backgroundColor: "black",
             vAxis: {
               title: "Number of Tasks completed",
