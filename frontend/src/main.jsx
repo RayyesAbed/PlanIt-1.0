f3e0a4672189ebefeac7f78be971dcb00e3b7998
RayyesAbed <abedray1567@gmail.com>
2025-04-07 15:26:35 +0200

    Initiated an ApolloClient for React, defined the URI as well as wrapped the whole app with ApolloProvider with client prop passed to it

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,13 +6,21 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs from "dayjs";
 import { MantineProvider } from "@mantine/core";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: `${import.meta.env.VITE_BACKEND_URL}/graphql`,
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MantineProvider>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <App />
-      </LocalizationProvider>
-    </MantineProvider>
+    <ApolloProvider client={client}>
+      <MantineProvider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <App />
+        </LocalizationProvider>
+      </MantineProvider>
+    </ApolloProvider>
   </StrictMode>
 );
