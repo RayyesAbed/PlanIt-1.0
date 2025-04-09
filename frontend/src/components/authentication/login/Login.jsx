76c5febe97cc7dca52418d3d98cbf6b8e983e3ed
RayyesAbed <abedray1567@gmail.com>
2025-04-09 13:38:50 +0200

    Created 2 useStates (logging in pending boolean and resetting password pending boolean) and used them to return conditional data for better user experience

--- frontend/src/components/authentication/login/Login.jsx
+++ frontend/src/components/authentication/login/Login.jsx
@@ -1,4 +1,4 @@
-import { TextField } from "@mui/material";
+import { CircularProgress, TextField } from "@mui/material";
 import styles from "./Login.module.css";
 import LoginSectionImg from "/LoginSectionImg.webp";
 import Logo from "/PlanItLogo.webp";
@@ -23,6 +23,8 @@ const Login = () => {
     email: "",
     password: "",
   });
+  const [isLoginPending, setIsLoginPending] = useState(false);
+  const [isPasswordResetPending, setIsPasswordResetPending] = useState(false);
 
   const handleChangeEmail = (event) =>
     setUserCredentials({ ...userCredentials, email: event.target.value });
@@ -34,19 +36,23 @@ const Login = () => {
 
   const loginUserHandler = async (event) => {
     event.preventDefault();
+    setIsLoginPending(true);
     const result = await loginUser(userCredentials);
     if (!result) {
       alert("Invalid email, password or both!");
+      setIsLoginPending(false);
     } else {
       navigate("/tasks");
     }
   };
 
   const resetUserRequestHandler = async () => {
+    setIsPasswordResetPending(true);
     await resetPasswordRequest(userCredentials.email);
     alert(
       "If the email you provided is registered, you will receive an email with instructions to reset your password."
     );
+    setIsPasswordResetPending(false);
   };
 
   return (
@@ -57,56 +63,67 @@ const Login = () => {
       <section>
         <form onSubmit={loginUserHandler}>
           <img src={Logo} alt="PlanIt Logo" />
-          <h1>Log in to your account</h1>
-          <TextField
-            type="email"
-            label="Enter your Email"
-            value={userCredentials.email}
-            onChange={handleChangeEmail}
-            sx={{ backgroundColor: "rgb(244, 244, 244)" }}
-            className={styles.loginInput}
-            required
-          />
-          <FormControl variant="outlined">
-            <InputLabel htmlFor="outlined-adornment-password">
-              Enter your Password
-            </InputLabel>
-            <OutlinedInput
-              className={styles.loginInput}
-              sx={{ backgroundColor: "rgb(244,244,244)" }}
-              id="outlined-adornment-password"
-              value={userCredentials.password}
-              onChange={handleChangePassword}
-              type={showPassword ? "text" : "password"}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label={
-                      showPassword
-                        ? "hide the password"
-                        : "display the password"
-                    }
-                    onClick={handleClickToggleShowPassword}
-                    edge="end"
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-              label="Enter your Password"
-              required
-            />
-          </FormControl>
-          <Link
-            className={styles.forgotPasswordLink}
-            onClick={resetUserRequestHandler}
-          >
-            Forgot your Password?
-          </Link>
-          <button className={styles.loginButton}>Login</button>
-          <Link className={styles.createAccountLink} to="/register">
-            Don&apos;t have an account? Then register
-          </Link>
+          {isLoginPending ? (
+            <>
+              <h1>Logging in...</h1>
+              <CircularProgress />
+            </>
+          ) : (
+            <>
+              <h1>Log in to your account</h1>
+              <TextField
+                type="email"
+                label="Enter your Email"
+                value={userCredentials.email}
+                onChange={handleChangeEmail}
+                sx={{ backgroundColor: "rgb(244, 244, 244)" }}
+                className={styles.loginInput}
+                required
+              />
+              <FormControl variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">
+                  Enter your Password
+                </InputLabel>
+                <OutlinedInput
+                  className={styles.loginInput}
+                  sx={{ backgroundColor: "rgb(244,244,244)" }}
+                  id="outlined-adornment-password"
+                  value={userCredentials.password}
+                  onChange={handleChangePassword}
+                  type={showPassword ? "text" : "password"}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label={
+                          showPassword
+                            ? "hide the password"
+                            : "display the password"
+                        }
+                        onClick={handleClickToggleShowPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Enter your Password"
+                  required
+                />
+              </FormControl>
+              <Link
+                className={styles.forgotPasswordLink}
+                onClick={resetUserRequestHandler}
+              >
+                {isPasswordResetPending
+                  ? "Sending a password reset email..."
+                  : "Forgot your Password?"}
+              </Link>
+              <button className={styles.loginButton}>Login</button>
+              <Link className={styles.createAccountLink} to="/register">
+                Don&apos;t have an account? Then register
+              </Link>
+            </>
+          )}
         </form>
       </section>
     </div>
