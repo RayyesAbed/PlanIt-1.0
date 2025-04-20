0717334e3c67ec7473488f9334f93ac9b26e1e83
RayyesAbed <abedray1567@gmail.com>
2025-04-20 22:47:19 +0200

    Added more content to be rendered in my story JSX component:
    - Added a add icon with a tooltip to invoke a dialog to enter desired goal, the dialog is to be created later
    - Animated 3 pieces of paragraphs for users that do not have a story for explanation
    - Used useState to store the current text index and to update the UI based on it
    - Used useEffect to update the current text index with a setTimeout function that gets called every 4 seconds

--- frontend/src/components/application/myStory/MyStory.jsx
+++ frontend/src/components/application/myStory/MyStory.jsx
@@ -1,12 +1,60 @@
+import { Tooltip } from "@mui/material";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 import UserPageMenu from "../common/userPageMenu/UserPageMenu";
 import styles from "./MyStory.module.css";
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
 
 const MyStory = () => {
   document.title = "My Story";
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+
+  const MESSAGES = [
+    "Welcome to the future you!",
+    "You are just one prompt away from setting your goals with AI!",
+    "Click on the + icon and enter your desired goal",
+  ]; // Used for animating text when the user does not have stories
+
+  // This effect will change the text every 4 seconds
+  useEffect(() => {
+    if (currentTextIndex < MESSAGES.length - 1) {
+      const interval = setTimeout(() => {
+        setCurrentTextIndex((prevIndex) => prevIndex + 1);
+      }, 4000);
+
+      return () => clearTimeout(interval);
+    }
+  });
+
   return (
     <div className={styles.wrapper}>
       <UserPageMenu />
-      <div className={styles.myStoryWrapper}></div>
+      <div className={styles.myStoryWrapper}>
+        <div className={styles.topTitleParagraphWrapper}>
+          <h1>My Story</h1>
+          <p>
+            Number of Stories: 0{/* Will be changed later with a real number */}
+          </p>
+        </div>
+        <div className={styles.myStoryContent}>
+          <AnimatePresence mode="wait">
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ delay: 0.5, duration: 1 }}
+              key={currentTextIndex}
+              className={styles.welcomeText}
+            >
+              {MESSAGES[currentTextIndex]}
+            </motion.p>
+          </AnimatePresence>
+
+          <Tooltip title="Create a Story">
+            <AddCircleIcon className={styles.createStoryIcon} />
+          </Tooltip>
+        </div>
+      </div>
     </div>
   );
 };
