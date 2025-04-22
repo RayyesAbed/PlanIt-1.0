755d25579d176b95121aebb6361271c5e5de6b79
RayyesAbed <abedray1567@gmail.com>
2025-04-22 20:24:49 +0200

    Added the form with the text area element, the button, and used useState to store and set the user target data on the frontend side, as well as added a close icon for exiting from the dialog

--- frontend/src/components/application/common/dialogs/createStory/CreateStoryDialog.jsx
+++ frontend/src/components/application/common/dialogs/createStory/CreateStoryDialog.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useRef, useState } from "react";
 import * as motion from "motion/react-client";
 import { AnimatePresence } from "motion/react";
+import CloseIcon from "@mui/icons-material/Close";
 import styles from "../TaskDialog.module.css";
 
 const CreateStoryDialog = ({ openModal, closeModal }) => {
   const ref = useRef(); // modal ref
+  const [userTarget, setUserTarget] = useState("");
 
   useEffect(() => {
     if (openModal) {
@@ -13,6 +15,10 @@ const CreateStoryDialog = ({ openModal, closeModal }) => {
       ref.current?.close();
     }
   });
+
+  const handleSubmitGoal = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <AnimatePresence initial={false}>
@@ -25,7 +31,31 @@ const CreateStoryDialog = ({ openModal, closeModal }) => {
           exit={{ opacity: 0, scale: 0 }}
           className={styles.taskDialog}
         >
-          <h2>Create Story</h2>
+          <div onClick={() => closeModal()} id={styles.closeModalDiv}>
+            <CloseIcon />
+          </div>
+          <h2>Create a Story</h2>
+          <form onSubmit={handleSubmitGoal}>
+            <div style={{ alignItems: "center" }}>
+              <textarea
+                style={{
+                  flexBasis: "100%",
+                  height: "150px",
+                  backgroundColor: "rgb(240,240,240)",
+                }}
+                value={userTarget}
+                onChange={(event) => setUserTarget(event.target.value)}
+                placeholder="Please enter your desired goal"
+              />
+            </div>
+            <button
+              className={styles.addTaskButton}
+              type="submit"
+              style={{ marginTop: "30px" }}
+            >
+              Create
+            </button>
+          </form>
         </motion.dialog>
       )}
     </AnimatePresence>
