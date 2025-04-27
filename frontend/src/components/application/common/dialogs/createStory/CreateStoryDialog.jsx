8f602c4b6eaa6c19cc21e95a8a2f1a4c9142bf5c
RayyesAbed <abedray1567@gmail.com>
2025-04-27 15:00:45 +0200

    Handled the goal submitting function with the newly created createStory function:
    - Passed the goal to the createStory function.
    - Used a boolean useState to check the loading state.
    - And returned conditional circular progress based on the loading state boolean value.

--- frontend/src/components/application/common/dialogs/createStory/CreateStoryDialog.jsx
+++ frontend/src/components/application/common/dialogs/createStory/CreateStoryDialog.jsx
@@ -3,10 +3,13 @@ import * as motion from "motion/react-client";
 import { AnimatePresence } from "motion/react";
 import CloseIcon from "@mui/icons-material/Close";
 import styles from "../TaskDialog.module.css";
+import { createStory } from "../../../../../api/story/createStory";
+import { CircularProgress } from "@mui/material";
 
 const CreateStoryDialog = ({ openModal, closeModal }) => {
   const ref = useRef(); // modal ref
   const [userTarget, setUserTarget] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (openModal) {
@@ -16,8 +19,14 @@ const CreateStoryDialog = ({ openModal, closeModal }) => {
     }
   });
 
-  const handleSubmitGoal = (event) => {
+  const handleSubmitGoal = async (event) => {
     event.preventDefault();
+    setIsLoading(true);
+    const responseData = await createStory(userTarget);
+    alert(responseData.message);
+    setIsLoading(false);
+    setUserTarget("");
+    closeModal();
   };
 
   return (
@@ -35,27 +44,40 @@ const CreateStoryDialog = ({ openModal, closeModal }) => {
             <CloseIcon />
           </div>
           <h2>Create a Story</h2>
-          <form onSubmit={handleSubmitGoal}>
-            <div style={{ alignItems: "center" }}>
-              <textarea
-                style={{
-                  flexBasis: "100%",
-                  height: "150px",
-                  backgroundColor: "rgb(240,240,240)",
-                }}
-                value={userTarget}
-                onChange={(event) => setUserTarget(event.target.value)}
-                placeholder="Please enter your desired goal"
-              />
-            </div>
-            <button
-              className={styles.addTaskButton}
-              type="submit"
-              style={{ marginTop: "30px" }}
+          {isLoading ? (
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginTop: "15%",
+              }}
             >
-              Create
-            </button>
-          </form>
+              <CircularProgress color="inherit" sx={{ marginRight: "20px" }} />
+              Creating your Future Self...
+            </div>
+          ) : (
+            <form onSubmit={handleSubmitGoal}>
+              <div style={{ alignItems: "center" }}>
+                <textarea
+                  style={{
+                    flexBasis: "100%",
+                    height: "150px",
+                    backgroundColor: "rgb(240,240,240)",
+                  }}
+                  value={userTarget}
+                  onChange={(event) => setUserTarget(event.target.value)}
+                  placeholder="Please enter your desired goal"
+                />
+              </div>
+              <button
+                className={styles.addTaskButton}
+                type="submit"
+                style={{ marginTop: "30px" }}
+              >
+                Create
+              </button>
+            </form>
+          )}
         </motion.dialog>
       )}
     </AnimatePresence>
