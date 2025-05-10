9340d70ef39ace610a1a4c5374e4fee2dd253f16
RayyesAbed <abedray1567@gmail.com>
2025-05-10 09:20:52 +0200

    Animated the story card when clicked and showed the story text along with story title with React Motion library

--- frontend/src/components/application/myStory/card/StoryCard.jsx
+++ frontend/src/components/application/myStory/card/StoryCard.jsx
@@ -1,10 +1,38 @@
+import { AnimatePresence, motion } from "motion/react";
 import styles from "./StoryCard.module.css";
 
-const StoryCard = ({ story }) => {
+const StoryCard = ({ story, flipped, onClick }) => {
   return (
-    <div className={styles.storyCard}>
-      <h3>{story.storyTitle}</h3>
-    </div>
+    <AnimatePresence mode="wait">
+      <motion.div
+        className={styles.storyCard}
+        onClick={onClick}
+        animate={
+          flipped
+            ? {
+                rotateY: 180,
+                width: "100%",
+                height: "55vh",
+              }
+            : {}
+        }
+      >
+        <motion.h3 animate={flipped ? { rotateY: 180 } : {}}>
+          {story.storyTitle}
+        </motion.h3>
+        {flipped && (
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+            exit={{ opacity: 0 }}
+            style={flipped ? { rotateY: 180 } : {}}
+          >
+            {story.storyText}
+          </motion.p>
+        )}
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
