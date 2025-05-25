7557ed4e08ddcbd57b88bb5dac08d8bb0ba0751f
RayyesAbed <abedray1567@gmail.com>
2025-05-25 18:54:32 +0200

    Handled the Photo edit field value and returned components based on that:
    - Returned the user avatar if they have an existing image and if they don't, then return a basic 'Me' avatar
    - Added an input to enable the users to add or update their avatar
    - If the user doesn't have an avatar, then show the add button
    - If the user has an avatar, then show the edit and delete buttons
    - Added a useRef to prevent re-renders and to access the input element directly
    - Added an avatar URL useState for previewing the user's avatar to be uploaded
    - Handled both the trigger file select and the handle file upload functions

--- frontend/src/components/application/settings/common/dialogs/components/GetSpecificDialogComponent.jsx
+++ frontend/src/components/application/settings/common/dialogs/components/GetSpecificDialogComponent.jsx
@@ -1,7 +1,32 @@
+import useFetchUserAvatar from "../../../../../../hooks/useFetchUserAvatar";
+import { Avatar, Box, IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import AddIcon from "@mui/icons-material/Add";
 import styles from "../SettingsDialog.module.css";
+import { useRef, useState } from "react";
 
 const GetSpecificDialogComponent = ({ type, name, defaultValue }) => {
   let content;
+
+  const [avatarUrl, setAvatarUrl] = useState();
+  const fileInputRef = useRef(null);
+
+  const userAvatar = useFetchUserAvatar();
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const previewUrl = URL.createObjectURL(file);
+      setAvatarUrl(previewUrl);
+    }
+  };
+
+  const triggerFileSelect = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
 
   if (name === "Email" || name === "Name") {
     content = (
@@ -25,6 +50,51 @@ const GetSpecificDialogComponent = ({ type, name, defaultValue }) => {
           required
         />
       </div>
+    );
+  } else if (name === "Photo") {
+    content = (
+      <Box className={styles.avatarBox}>
+        {userAvatar || avatarUrl ? (
+          <Avatar
+            alt="Your avatar"
+            src={userAvatar ? userAvatar : avatarUrl}
+            className={styles.userAvatar}
+          />
+        ) : (
+          <Avatar className={styles.userAvatar}>Me</Avatar>
+        )}
+        <input
+          name="avatar"
+          ref={fileInputRef}
+          type="file"
+          accept="image/*"
+          style={{ display: "none" }}
+          onChange={handleFileUpload}
+        />
+        {!userAvatar && (
+          <IconButton
+            size="small"
+            className={styles.addAvatarButton}
+            onClick={triggerFileSelect}
+          >
+            <AddIcon fontSize="small" />
+          </IconButton>
+        )}
+        {userAvatar && (
+          <IconButton
+            size="small"
+            className={styles.editAvatarButton}
+            onClick={triggerFileSelect}
+          >
+            <EditIcon fontSize="small" />
+          </IconButton>
+        )}
+        {userAvatar && (
+          <IconButton size="small" className={styles.deleteAvatarButton}>
+            <DeleteIcon fontSize="small" />
+          </IconButton>
+        )}
+      </Box>
     );
   }
 
