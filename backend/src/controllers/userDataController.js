8ac3df7956f3a8c28220831dabadac462e60091e
RayyesAbed <abedray1567@gmail.com>
2025-05-27 03:58:30 +0200

    Created an upload user avatar in the backend to Amazon S3:
    - Checked for token existence
    - Made sure that the file actual type is image
    - Used the PutObjectCommand to send the request to S3
    - Then returned success message on successful image upload

--- backend/src/controllers/userDataController.js
+++ backend/src/controllers/userDataController.js
@@ -1,5 +1,10 @@
 const verifyToken = require("../services/verifyToken");
 const User = require("../schemas/User");
+const fs = require("fs");
+const fileType = require("file-type");
+const path = require("path");
+const s3 = require("../configs/s3Connect");
+const { PutObjectCommand } = require("@aws-sdk/client-s3");
 
 const getUserData = async (req, res, next) => {
   try {
@@ -37,4 +42,40 @@ const getUserData = async (req, res, next) => {
   }
 };
 
-module.exports = getUserData;
+const uploadUserAvatar = async (req, res) => {
+  if (!req.headers.cookie) {
+    return res.sendStatus(401);
+  }
+
+  const token = req.headers.cookie.split("=")[1];
+
+  // if there is no token
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  const { userId } = verifyToken(token);
+
+  const fileBuffer = req.file.buffer;
+  const type = await fileType.fromBuffer(fileBuffer);
+
+  if (!type || !type.mime.startsWith("image/")) {
+    return res.status(400).json({ message: "Only image types are allowed" });
+  }
+
+  const command = new PutObjectCommand({
+    Bucket: process.env.AWS_S3_BUCKET,
+    Key: `avatars/${userId}`,
+    Body: fileBuffer,
+    ContentType: type.mime,
+  });
+
+  await s3.send(command);
+
+  return res.status(201).json({ message: "Uploaded successfully!" });
+};
+
+module.exports = {
+  uploadUserAvatar,
+  getUserData,
+};
