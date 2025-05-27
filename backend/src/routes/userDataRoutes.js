2f27a541f5107471700def178790ff4fb31d0edc
RayyesAbed <abedray1567@gmail.com>
2025-05-27 04:46:55 +0200

    Passed the get user avatar as a middleware in its respective API endpoint

--- backend/src/routes/userDataRoutes.js
+++ backend/src/routes/userDataRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   getUserData,
   uploadUserAvatar,
+  getUserAvatar,
 } = require("../controllers/userDataController");
 const upload = require("../configs/multer");
 const router = express.Router();
@@ -9,5 +10,7 @@ const router = express.Router();
 router.get("/", getUserData);
 
 router.post("/upload_avatar", upload.single("avatar"), uploadUserAvatar);
+
+router.get("/get_avatar", getUserAvatar);
 
 module.exports = router;
