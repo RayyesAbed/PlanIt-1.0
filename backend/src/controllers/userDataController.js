2b0dd3020b04899b45921f0d94a9431bcf6fc8e5
RayyesAbed <abedray1567@gmail.com>
2025-05-27 04:43:36 +0200

    Created a function for getting user avatar, generating a temporary URL for that user, and returning that URL:
    - Checked for token existence
    - Created a head params since initially we need to look for the user avatar if it exists
    - Then if found we get the avatar with the GetObjectCommand
    - Afterwards we generate a signed URL and return it in the response

--- backend/src/controllers/userDataController.js
+++ backend/src/controllers/userDataController.js
@@ -4,7 +4,12 @@ const fs = require("fs");
 const fileType = require("file-type");
 const path = require("path");
 const s3 = require("../configs/s3Connect");
-const { PutObjectCommand } = require("@aws-sdk/client-s3");
+const {
+  PutObjectCommand,
+  GetObjectCommand,
+  HeadObjectCommand,
+} = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 const getUserData = async (req, res, next) => {
   try {
@@ -75,7 +80,38 @@ const uploadUserAvatar = async (req, res) => {
   return res.status(201).json({ message: "Uploaded successfully!" });
 };
 
+const getUserAvatar = async (req, res) => {
+  if (!req.headers.cookie) {
+    return res.sendStatus(401);
+  }
+
+  const token = req.headers.cookie.split("=")[1];
+
+  // if there is no token
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  const { userId } = verifyToken(token);
+
+  const headParams = {
+    Bucket: process.env.AWS_S3_BUCKET,
+    Key: `avatars/${userId}`,
+  };
+
+  try {
+    await s3.send(new HeadObjectCommand(headParams)); // First check if the object exists in the bucket
+    const getParams = { ...headParams }; // If exists, then copy the head params to the get params object
+    const command = new GetObjectCommand(getParams);
+    const url = await getSignedUrl(s3, command, { expiresIn: 3600 }); // Then generate the signed URL
+    return res.status(200).json({ url: url });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to generate signed URL" });
+  }
+};
+
 module.exports = {
   uploadUserAvatar,
   getUserData,
+  getUserAvatar,
 };
