5369853c3d53b77e6225bf6352131d84d0842f99
RayyesAbed <abedray1567@gmail.com>
2025-06-03 13:36:34 +0200

    Wrapped my whole app with the user data provider, as well as deleted dayjs import since I do not need it here

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,9 @@ import "./index.css";
 import App from "./App.jsx";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
 import { MantineProvider } from "@mantine/core";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import UserDataProvider from "./providers/UserDataProvider.jsx";
 
 export const client = new ApolloClient({
   uri: `${import.meta.env.VITE_BACKEND_URL}/graphql`,
@@ -18,7 +18,9 @@ createRoot(document.getElementById("root")).render(
     <ApolloProvider client={client}>
       <MantineProvider>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <App />
+          <UserDataProvider>
+            <App />
+          </UserDataProvider>
         </LocalizationProvider>
       </MantineProvider>
     </ApolloProvider>
