f40bb3425b0b7640d0e296ba8f879b02e28066bd
RayyesAbed <abedray1567@gmail.com>
2025-06-03 20:16:51 +0200

    Used the useCallback hook to only change when the user data or image changes, and then used useEffect to load the data when the component is mounted

--- frontend/src/providers/UserDataProvider.jsx
+++ frontend/src/providers/UserDataProvider.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { UserDataContext } from "../contexts/UserDataContext";
 import { fetchUserData } from "../api/user/fetchUserData";
 import { getAvatar } from "../api/user/getAvatar";
@@ -6,17 +6,19 @@ import { getAvatar } from "../api/user/getAvatar";
 const UserDataProvider = ({ children }) => {
   const [userData, setUserData] = useState({ data: null, image: null });
 
-  useEffect(() => {
-    const loadUser = async () => {
-      const data = await fetchUserData();
-      const image = await getAvatar();
-      setUserData({ data: data, image: image });
-    };
-    loadUser();
+  const loadUserData = useCallback(async () => {
+    const data = await fetchUserData();
+    const image = await getAvatar();
+    setUserData({ data, image });
   }, []);
 
+  // load once on mount
+  useEffect(() => {
+    loadUserData();
+  }, [loadUserData]);
+
   return (
-    <UserDataContext.Provider value={{ userData, setUserData }}>
+    <UserDataContext.Provider value={{ userData, setUserData, loadUserData }}>
       {children}
     </UserDataContext.Provider>
   );
